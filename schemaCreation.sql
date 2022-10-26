49ff7474666f6eb7514fa96b756b84a24ffb929f
willpav <116762472+willpav@users.noreply.github.com>
2022-10-26 16:31:51 -0400

    add book table

--- schemaCreation.sql
+++ schemaCreation.sql
@@ -1,1 +1,14 @@
-CREATE TABLE
+CREATE TABLE BOOK (
+    Title varchar(25),
+    Author varchar(40),
+    Genre varchar(25),
+    ReleaseDate date,
+    ISBN char(13),
+    InventoryID varchar(6),
+    Price decimal,
+    Cost decimal,
+    StoreID varchar(4),
+    ShipmentID varchar(4),
+    TransactionID varchar(4),
+    PublisherID varchar(4)
+);
